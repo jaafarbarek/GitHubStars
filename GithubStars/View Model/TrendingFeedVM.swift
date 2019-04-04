2866648a02403c4fdfd5f8bf84548a59fe38bb67
jaafar <jaafar@atfold.com>
2019-04-04 12:50:48 +0300

    Added Trending Feed View Model

---
+++ GithubStars/View Model/TrendingFeedVM.swift
@@ -0,0 +1,57 @@
+//
+//  TrendingFeedVM.swift
+//  GithubStars
+//
+//  Created by Jaafar Barek on 4/4/19.
+//  Copyright © 2019 Jaafar Barek. All rights reserved.
+//
+
+import RxSwift
+
+class TrendingFeedVM {
+    
+    // MARK: Current api service
+    let gitAPIService: RepoService
+    
+    // MARK: Rx Bindings & DisposeBag
+    var disposeBag = DisposeBag()
+    let downloadTrigger = PublishSubject<Void>()
+    let repos = PublishSubject<[GitRepository]>()
+    
+    init(gitAPIService: RepoService) {
+        self.gitAPIService = gitAPIService
+        initRepos()
+    }
+    
+    var allRepos = [GitRepository]()
+    
+    // MARK: Initialization subscription for [Repo]
+    func initRepos() {
+        downloadTrigger
+            .subscribe(onNext: { [unowned self] in
+                
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd"
+                let dateString = formatter.string(from: Date().addingTimeInterval(-30*24*3600))
+                
+                let path = "search/repositories?q=created:&sort=stars&order=desc"
+                //                let urlString = self.gitAPIService.baseURL.absoluteString + "search/repositories?q=created:>\(dateString)"
+                //                let urlString = self.gitAPIService.baseURL.absoluteString + "search/repositories?q=created:>\(dateString)&sort=stars&order=desc"//.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+                //                let urlString = self.gitAPIService.baseURL.absoluteString + "search/repositories?q=created:>\(dateString)&sort=stars&order=desc"
+                self.gitAPIService.page += 1
+                
+                DispatchQueue.global(qos: .background).async {
+                    self.gitAPIService.getRepos(path: path)
+                        .observeOn(MainScheduler.instance)
+                        .subscribe(onNext: { [unowned self] repos in
+                            self.allRepos += repos
+                            self.repos.onNext(self.allRepos)
+                        })
+                        .disposed(by: self.disposeBag)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+}
+
